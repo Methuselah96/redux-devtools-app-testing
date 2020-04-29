7dd95f3458621f138e38f69738b21f7309ae9af2
Nathan Bierema <nbierema@gmail.com>
2020-04-28 21:24:00 -0400

    Use connect and trace option

--- default-redux-template/src/index.js
+++ default-redux-template/src/index.js
@@ -6,6 +6,10 @@ import store from './app/store';
 import { Provider } from 'react-redux';
 import * as serviceWorker from './serviceWorker';
 
+window.__REDUX_DEVTOOLS_EXTENSION__.connect({
+  trace: true,
+});
+
 ReactDOM.render(
   <React.StrictMode>
     <Provider store={store}>
