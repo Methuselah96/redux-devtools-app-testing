2d6ebc1099153ed8ab94ed352f826d6fe546f772
Nathan Bierema <nbierema@gmail.com>
2020-04-28 21:31:57 -0400

    Updates

---
+++ precache-manifest.129990648addb4d9cc2157845f728067.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a892379892bb180db29024137941bd12",
+    "url": "/connect-trace/index.html"
+  },
+  {
+    "revision": "7138e2b325cad61a31a5",
+    "url": "/connect-trace/static/css/main.f559617d.chunk.css"
+  },
+  {
+    "revision": "dc32febeea8aac288e22",
+    "url": "/connect-trace/static/js/2.7493bf53.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/connect-trace/static/js/2.7493bf53.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7138e2b325cad61a31a5",
+    "url": "/connect-trace/static/js/main.bf8c0197.chunk.js"
+  },
+  {
+    "revision": "9460aa7163ca804e23d3",
+    "url": "/connect-trace/static/js/runtime-main.1b82fdda.js"
+  },
+  {
+    "revision": "8dad80289a1ece380591ad143b616747",
+    "url": "/connect-trace/static/media/logo.8dad8028.svg"
+  }
+]);
